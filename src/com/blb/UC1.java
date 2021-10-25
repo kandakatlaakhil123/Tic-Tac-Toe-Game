98eef5587efd0f6265277f7cf0575df9bc4c0c14
kandakatlaakhil123 <91108116+kandakatlaakhil123@users.noreply.github.com>
2021-10-25 20:39:17 +0530

    added: UC1 is added

--- src/com/blb/UC1.java
+++ src/com/blb/UC1.java
@@ -1,5 +1,7 @@
 package com.blb;
 
 public class UC1 {
-    System.out.println("Welcome to game");
+    public static void main(String[] args) {
+        System.out.println("Tic tac toe game");
+    }
 }
